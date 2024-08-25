8edbd987b5e94e53a545fe1d716e6797bccf33e3
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-08-25 12:43:43 +0300

    Revert "move files"
    This reverts commit 3ba9fac59be75771c640d8bc3b5cf0f341b321e8.

--- server/runPythonScript.js
+++ server/runPythonScript.js
@@ -1,22 +1,24 @@
 const { exec } = require('child_process');
 const path = require('path');
 
-const ffpython = path.join(__dirname, '../FontForgeBuilds', 'bin', 'ffpython');
+const ffpython = path.join(__dirname, './FontForgeBuilds', 'bin', 'ffpython');
 
 function runPythonScript(scriptPath, args = [], callback) {
   const command = `${ffpython} ${scriptPath} ${args.join(' ')}`;
+
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
-      callback(null, error.message);
+      return callback(error);
     }
     if (stderr) {
       console.error(`stderr: ${stderr}`);
+      return callback(new Error(stderr));
     }
-    if (stdout) {
-      const outputFontPath = stdout.trim();
-      callback(null, outputFontPath);
-    }
+
+    // Assuming the output font path is returned by the Python script as the last line in stdout
+    const outputFontPath = stdout.trim(); // Ensure that the path is extracted correctly
+    callback(null, outputFontPath);
   });
 }
 
