33cb0611af72a7b7099f9acf8894afc34618aee9
Maurvick <82706197+Maurvick@users.noreply.github.com>
2024-10-22 13:23:24 +0300

    override eslint rules

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -2,6 +2,19 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      // Override `no-unused-vars` to show warnings instead of errors
+      'no-unused-vars': 'warn',
+    },
+  },
 ];
